da57d56526b435ecc28c3a1b3ebe3f06deb5b4f6
Adam Keenan <adam.g.keenan@gmail.com>
2021-02-01 10:27:51 -0500

    Bump version to 1.4.0

--- DejaVu.cpp
+++ DejaVu.cpp
@@ -15,7 +15,7 @@
 
 INITIALIZE_EASYLOGGINGPP
 
-BAKKESMOD_PLUGIN(DejaVu, "Deja Vu", "1.3.9", 0)
+BAKKESMOD_PLUGIN(DejaVu, "Deja Vu", "1.4.0", 0)
 
 template <class T>
 CVarWrapper DejaVu::RegisterCVar(
